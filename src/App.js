26ef6c16dc26622d755900043528ff3572149d35
Vachan Gungewale <vachan.gungewale97@gmail.com>
2023-02-06 10:48:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Pagination from "./Pagination";
+import Search from "./Search";
+import Stories from "./Stories";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Search />
+      <Pagination />
+      <Stories />
+    </>
+  );
+}
+
+export default App;
